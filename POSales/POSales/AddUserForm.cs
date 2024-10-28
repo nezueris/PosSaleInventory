318ab54b21fd9f5c251e87f9c1c346cab0620e6c
nezueris <dreymamuri@gmail.com>
2024-10-28 20:15:46 +0800

    Initial commit for POSales project

---
+++ POSales/POSales/AddUserForm.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace POSales
+{
+    public partial class AddUserForm : Form
+    {
+        SqlConnection cn;
+        SqlCommand cm;
+        DBConnect dbcon = new DBConnect();
+        public AddUserForm()
+        {
+            InitializeComponent();
+            cn = new SqlConnection(dbcon.myConnection());
+
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            if (txtPassword.Text != txtRePassword.Text)
+            {
+                MessageBox.Show("Passwords do not match!", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            try
+            {
+                cn.Open();
+                cm = new SqlCommand("INSERT INTO tbUser (username, password, role, name) VALUES (@username, @password, @role, @name)", cn);
+                cm.Parameters.AddWithValue("@username", txtUsername.Text);
+                cm.Parameters.AddWithValue("@password", txtPassword.Text);
+                cm.Parameters.AddWithValue("@role", cbRole.Text);
+                cm.Parameters.AddWithValue("@name", txtName.Text);
+                cm.ExecuteNonQuery();
+                cn.Close();
+                MessageBox.Show("New user added successfully!", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                this.DialogResult = DialogResult.OK;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        
+    }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
